d0422c77992cd5a31731cea4ec5a98a5e84cb99f
GRUPOSOGECABLE\pprietom <pablo.prietomartin.ext@telefonica.com>
2018-03-12 11:20:12 +0100

    Added ResultFoodBookedListenner as new var and deleted the implementation in the class.

--- app/src/main/java/com/example/pprietom/restaurantmvp/restaurant/RestaurantPresenterImp.java
+++ app/src/main/java/com/example/pprietom/restaurantmvp/restaurant/RestaurantPresenterImp.java
@@ -6,7 +6,7 @@ import com.example.pprietom.restaurantmvp.R;
  * Created by Pabel on 09/03/2018.
  **/
 
-public class RestaurantPresenterImp implements RestaurantPresenter, RestaurantInteractor.ResultFoodBookedListener{
+public class RestaurantPresenterImp implements RestaurantPresenter {
 
     /*****VARIABLES******/
     //region Variables
@@ -14,48 +14,64 @@ public class RestaurantPresenterImp implements RestaurantPresenter, RestaurantIn
     private RestaurantInteractor restaurantInteractor;
     //endregion
 
-    public RestaurantPresenterImp(BossRestaurantView bossRestaurantView) {
+    RestaurantPresenterImp(BossRestaurantView bossRestaurantView) {
         this.bossRestaurantView = bossRestaurantView;
         this.restaurantInteractor = new RestaurantInteractorImp();
     }
 
     @Override
     public void bookYourFood(String s) {
-        if(checkBookFoodIsCorrect(s)){
-            restaurantInteractor.cookFood(s,this);
-        }else if (numberOfWords(s)==0){
+        //If the booking of the food send the error to de interface and return
+        if (!checkBookFoodIsCorrect(s)) {
+            returnError(s);
+            return;
+        }
+
+        restaurantInteractor.cookFood(s, new RestaurantInteractor.ResultFoodBookedListener() {
+            @Override
+            public void foodBookedIsCorrect(int foodCooked) {
+                bossRestaurantView.setRecipeCooked(foodCooked);
+            }
+
+            @Override
+            public void foodBookedIsWrong(int foodNotCooked) {
+                bossRestaurantView.setRecipeError(foodNotCooked);
+            }
+        });
+    }
+
+
+    /*+++++++++++++Axuliar+++++++++++*/
+    //region
+
+    /**
+     * This method ckeck that there are a minim food for cook
+     *
+     * @param bookFood get from the Editext food that user had writen
+     * @return if is correct the booking
+     */
+    private boolean checkBookFoodIsCorrect(String bookFood) {
+        return numberOfWords(bookFood) > 1;
+    }
+
+    /**
+     * This method set the response Error on the Interface
+     *
+     * @param s string that have writen the user.
+     */
+    private void returnError(String s) {
+        if (numberOfWords(s) == 0) {
             bossRestaurantView.setEditextEmptyError(R.string.error_sentence_empty);
         } else {
             bossRestaurantView.setRecipeError(R.string.error_words);
         }
     }
 
-    @Override
-    public void foodBookedIsCorrect(int foodCooked) {
-        bossRestaurantView.setRecipeCooked(foodCooked);
-    }
-
-    @Override
-    public void foodBookedIsWrong(int foodNotCooked) {
-        bossRestaurantView.setRecipeError(foodNotCooked);
-    }
-
-    /************Axuliar**********/
-    //region
-    /**
-     * This method ckeck that there are a minim food for cook
-     * @param bookFood get from the Editext food that user had writen
-     * @return if is correct the booking
-     */
-    private boolean checkBookFoodIsCorrect(String bookFood){
-        return numberOfWords(bookFood) > 1;
-    }
-
     /**
      * This method return the number of words of a sentence
      * TODO ¿Could be a Extension of String in kt?
      */
-    private int numberOfWords (String sentence){
+    private int numberOfWords(String sentence) {
         String trim = sentence.trim();
         if (trim.isEmpty())
             return 0;
